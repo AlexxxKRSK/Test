e27033c4da2dc5447cd165748cb558d5031bd4df
aplotnikov <aero2006@mail.ru>
2021-02-20 13:58:07 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import javax.swing.*;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Test");
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (Exception e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+//            BaseEntity
+        }
+    }
+}
